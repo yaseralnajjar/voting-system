a21631bd19dd73980b18ee4ffabe53e0300ac15b
YaserAlnajjar <kasseryasser@windowslive.com>
2019-05-24 02:14:46 +0300

    'deploy'

---
+++ precache-manifest.3c2fc7938a6edf9578711ce0615f3a04.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "05cb6825abf9e4691144",
+    "url": "/voting-system/static/js/chunk-vendors.9f906867.js"
+  },
+  {
+    "revision": "dd413d446e780bdab452",
+    "url": "/voting-system/static/js/app.25716d80.js"
+  },
+  {
+    "revision": "b2050f4bdebe0e44899cec81c27086c1",
+    "url": "/voting-system/static/img/bankid.b2050f4b.png"
+  },
+  {
+    "revision": "bf36f61afb2d1d9a201a0e681189c7b2",
+    "url": "/voting-system/static/img/4.bf36f61a.jpg"
+  },
+  {
+    "revision": "888e4fda4337570920190568144a6dc1",
+    "url": "/voting-system/static/img/3.888e4fda.jpg"
+  },
+  {
+    "revision": "d7cbc49cf5975249d18b89de77e9a8e9",
+    "url": "/voting-system/static/img/2.d7cbc49c.jpg"
+  },
+  {
+    "revision": "8f99e354b52e3a1ed509efb08b53b83f",
+    "url": "/voting-system/static/img/1.8f99e354.jpg"
+  },
+  {
+    "revision": "05cb6825abf9e4691144",
+    "url": "/voting-system/static/css/chunk-vendors.c4f52712.css"
+  },
+  {
+    "revision": "dd413d446e780bdab452",
+    "url": "/voting-system/static/css/app.d39313ab.css"
+  },
+  {
+    "revision": "7ee25f9304987c4397ada95a943cbddb",
+    "url": "/voting-system/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,29 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/voting-system/precache-manifest.3c2fc7938a6edf9578711ce0615f3a04.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "django-vue"});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
