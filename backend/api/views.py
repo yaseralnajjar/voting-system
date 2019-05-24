7206194fbad9c1b666245ba8852a8af61e2f286d
YaserAlnajjar <kasseryasser@windowslive.com>
2019-05-24 19:49:16 +0300

    Update backend views

--- backend/api/views.py
+++ backend/api/views.py
@@ -17,3 +17,40 @@ class MessageViewSet(viewsets.ModelViewSet):
     serializer_class = MessageSerializer
 
 
+def get_proxies():
+    data_provider = Connection(connection_string)
+    result = data_provider.query_proxies('SELECT * FROM Proxies')
+    return result
+
+
+def authorize_voter(proxy, voter):
+    key = RSA.generate(2048)
+    private_key = voter.private_key
+    message = str.encode(message)
+
+    rsa_public_key = RSA.importKey(public_key)
+    rsa_public_key = PKCS1_OAEP.new(rsa_public_key)
+    encrypted_text = rsa_public_key.encrypt(message)
+
+    if proxy.key == rsa_public_key:
+        return True
+    else:
+        return False
+
+
+def confirm_data(voter):
+    if voter.is_data_accepted():
+        return True
+    else:
+        return False
+
+
+def get_candidates():
+    data_provider = Connection(connection_string)
+    result = data_provider.query_proxies('SELECT * FROM Candidates')
+    return result
+
+
+def vote(voter, candidate):
+    data_provider = Connection(connection_string)
+    result = data_provider.query_proxies(f'INSERT INTO Votes VALUES ({voter.id}, {candidate.id})')
